bf88ad953eae932e82d58c9b9126c89190c154f7
Jesus <Jenriquez90@live.com>
2015-12-12 00:28:15 -0800

    Some changes

--- Scripts/Game/Main.js
+++ Scripts/Game/Main.js
@@ -10,47 +10,60 @@ define(function (require) {
     var canvas4 = document.getElementById('gameCanvas_4');
     var canvasContext4 = canvas4.getContext('2d');
 
-    var clouds = new GameElement(1800, 131, 'Images/Clouds.png', 0, 0, canvasContext1 );
-    var mountains = new GameElement(1800, 131, 'Images/Mountains.png', 0, 100, canvasContext2 );
-    var ground = new GameElement(1800, 50, 'Images/Ground.png', 0, 350, canvasContext3 );
-    var kakachu = new GameElement(75, 75, 'Images/Kakachu.png', 0, 280, canvasContext4 );
+    function updatePosition(){
+        this.x--;
+    }
 
+    var clouds = new GameElement(1800, 131, 'Images/Clouds.png', 0, 0, canvasContext1, updatePosition);
+    var mountains = new GameElement(1800, 131, 'Images/Mountains.png', 0, 100, canvasContext2, updatePosition);
+    var ground = new GameElement(1800, 50, 'Images/Ground.png', 0, 350, canvasContext3, updatePosition);
+    var kakachu = new GameElement(75, 75, 'Images/Kakachu.png', 0, 280, canvasContext4);
 
     var jumping = false;
-    var counter = 0;
-    setTimeout(function test(){
-        if(counter == 5){
-            canvasContext4.clearRect(0,0,600,400);
+    var counter = 0, DRAW_INTERVAL = 17;
+
+    setTimeout(function drawAll() {
+
+        if (counter == 5) {
+            canvasContext4.clearRect(0, 0, canvasContext4.canvas.width, canvasContext4.canvas.height);
             kakachu.draw();
         }
 
-        if(counter%30 == 0){
-            canvasContext1.clearRect(0,0,600,400);
-            canvasContext1.rect(0,0,600,400);
-            canvasContext1.fillStyle="lightblue";
-            canvasContext1.fill();
-            clouds.x = clouds.x - 1;
+        if (counter % 30 == 0) {
+            canvasContext1.clearRect(0, 0, canvasContext1.canvas.width, canvasContext1.canvas.height);
+            /*canvasContext1.rect(0,0,600,400);
+             canvasContext1.fillStyle="lightblue";
+             canvasContext1.fill();
+             */
+            clouds.updatePosition();
+            //clouds.x = clouds.x - 1;
             clouds.draw();
             counter = 0;
         }
-        if(counter % 10 == 0){
-            canvasContext2.clearRect(0,0,600,400);
-            mountains.x = mountains.x - 1;
+
+        if (counter % 10 == 0) {
+            canvasContext2.clearRect(0, 0, canvasContext2.canvas.width, canvasContext2.canvas.height);
+            mountains.updatePosition();
+            //mountains.x = mountains.x - 1;
             mountains.draw();
         }
-        if(counter % 2 == 0){
-            canvasContext3.clearRect(0,0,600,400);
-            ground.x = ground.x-.5;
+
+        if (counter % 4 == 0) {
+            canvasContext3.clearRect(0, 0, canvasContext3.canvas.width, canvasContext3.canvas.height);
+            ground.updatePosition();
+            //ground.x = ground.x - 1;
             ground.draw();
         }
-        if(jumping){
-            canvasContext4.clearRect(0,0,600,400);
+
+        if (jumping) {
+            canvasContext4.clearRect(0, 0, canvasContext4.canvas.width, canvasContext4.canvas.height);
             kakachu.draw();
         }
-        counter++;
-        setTimeout(test,17);
-    },17);
 
+        counter++;
+        setTimeout(drawAll, DRAW_INTERVAL);
+
+    }, DRAW_INTERVAL);
 
     canvas4.addEventListener('keydown', function (event) {
         if (event.keyCode === 32 && !jumping) {
@@ -60,10 +73,10 @@ define(function (require) {
                 x = Math.PI + (interval * counter++);
                 posY = 280 + 100 * Math.sin(x);
                 kakachu.y = posY;
-                if (x >= 2 * Math.PI){
+                if (x >= 2 * Math.PI) {
                     clearInterval(thisInterval);
                     jumping = false;
-                    canvasContext4.clearRect(0,0,600,400);
+                    canvasContext4.clearRect(0, 0, canvasContext4.canvas.width, canvasContext4.canvas.height);
                     kakachu.draw();
                 }
             }, 10)
