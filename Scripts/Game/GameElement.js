3e7382c74b159022f21728ef4f454b59bef6d6da
Jesus <Jenriquez90@live.com>
2015-12-12 00:29:35 -0800

    Some changes

--- Scripts/Game/GameElement.js
+++ Scripts/Game/GameElement.js
@@ -1,11 +1,16 @@
 define(function(){
-    function GameElement(width, height, src, x,y,ctx){
+    function GameElement(width, height, src, x,y,ctx, updateFn){
         this.width = width;
         this.height = height;
         this.image = new Image();
         this.image.src = src;
         this.x = x || 0;
         this.y = y || 0;
+        this.context = ctx;
+        this.canvas = ctx.canvas;
+
+        this.updatePosition = updateFn
+
         this.draw = function(){
             if(-this.width + ctx.canvas.clientWidth >= this.x ){
                 this.x = 0;
