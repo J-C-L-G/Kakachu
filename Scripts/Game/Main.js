8c29eb27aeea3e44bb7a4206b21751c854a987ea
Jesus <Jenriquez90@live.com>
2015-12-13 11:57:23 -0800

    Removed background canvases and added divs.

--- Scripts/Game/Main.js
+++ Scripts/Game/Main.js
@@ -3,40 +3,42 @@ define(function (require) {
     var EnemyManager = require('./EnemyManager');
     var Util = require('./Util');
 
-    var canvas1 = document.getElementById('gameCanvas_1'); //Clouds
-    var canvas2 = document.getElementById('gameCanvas_2'); //Mountains
-    var canvas3 = document.getElementById('gameCanvas_3'); //Ground
+    //var canvas1 = document.getElementById('gameCanvas_1'); //Clouds
+    //var canvas2 = document.getElementById('gameCanvas_2'); //Mountains
+    //var canvas3 = document.getElementById('gameCanvas_3'); //Ground
     var canvas4 = document.getElementById('gameCanvas_4'); //Enemies
     var canvas5 = document.getElementById('gameCanvas_5'); //Kakachu
 
     var enemyManager = new EnemyManager(canvas4);
-    var clouds = new GameElement({
-        width: 1800,
-        height: 131,
-        imageSource: 'Images/Clouds.png',
-        x: 0,
-        y: 0,
-        canvas: canvas1,
-        updateFn: Util.slideBG(1)
-    });
-    var mountains = new GameElement({
-        width: 1800,
-        height: 131,
-        imageSource: 'Images/Mountains.png',
-        x: 0,
-        y: 100,
-        canvas: canvas2,
-        updateFn: Util.slideBG(1)
-    });
-    var ground = new GameElement({
-        width: 1800,
-        height: 50,
-        imageSource: 'Images/Ground.png',
-        x: 0,
-        y: 350,
-        canvas: canvas3,
-        updateFn: Util.slideBG(1)
-    });
+
+    /*var clouds = new GameElement({
+     width: 1800,
+     height: 131,
+     imageSource: 'Images/Clouds.png',
+     x: 0,
+     y: 0,
+     canvas: canvas1,
+     updateFn: Util.slideBG(1)
+     });
+     var mountains = new GameElement({
+     width: 1800,
+     height: 131,
+     imageSource: 'Images/Mountains.png',
+     x: 0,
+     y: 100,
+     canvas: canvas2,
+     updateFn: Util.slideBG(1)
+     });
+     var ground = new GameElement({
+     width: 1800,
+     height: 50,
+     imageSource: 'Images/Ground.png',
+     x: 0,
+     y: 350,
+     canvas: canvas3,
+     updateFn: Util.slideBG(1)
+     });*/
+
     var kakachu = new GameElement({
         width: 75,
         height: 75,
@@ -56,22 +58,22 @@ define(function (require) {
         if (counter == 5) {
             kakachu.draw(true);
         }
-            if (counter % 30 == 0) {
-                if(Math.random() < 0.1){
-                    enemyManager.addEnemy();
-                }
-                clouds.draw(true);
-                counter = 0;
+        if (counter % 30 == 0) {
+            if (Math.random() < 0.1) {
+                enemyManager.addEnemy();
             }
-                if (counter % 10 == 0) {
-                    mountains.draw(true);
-                }
-                    if (counter % 4 == 0) {
-                        ground.draw(true);
-                    }
-                        if (jumping) {
-                            kakachu.draw(true);
-                        }
+            //clouds.draw(true);
+            counter = 0;
+        }
+        /*if (counter % 10 == 0) {
+         mountains.draw(true);
+         }
+         if (counter % 4 == 0) {
+         ground.draw(true);
+         }*/
+        if (jumping) {
+            kakachu.draw(true);
+        }
         enemyManager.drawAll();
         counter++;
         setTimeout(drawAll, DRAW_INTERVAL);
